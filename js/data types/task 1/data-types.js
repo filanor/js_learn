7742bc77c655d11ea860159e41e6816abaa6fd70
Filanor <filanor@gmail.com>
2023-01-25 01:46:46 +0300

    Модуль 1. Типы Данных

---
+++ js/data types/task 1/data-types.js
@@ -0,0 +1,31 @@
+// Number
+const age = 20;
+console.log("age", age, typeof age);
+
+// String
+const name = "John";
+console.log("name", name, typeof name);
+
+// Simbol
+const symbol = Symbol("id");
+console.log("symbol", symbol, typeof symbol);
+
+// Null
+const nullObject = null;
+console.log("nullObject", nullObject, typeof nullObject);
+
+// Undefined
+const undefinedObject = undefined;
+console.log("undefinedObject", undefinedObject, typeof undefinedObject);
+
+// Boolean
+const booleanObject = true;
+console.log("booleanObject", booleanObject, typeof booleanObject);
+
+// Object
+const objectObject = { name: "John", age: 20 };
+console.log("objectObject", objectObject, typeof objectObject);
+
+// BigInteger
+const bigIntegerObject = BigInt("1234567890123456789012345678901234567890");
+console.log("bigIntegerObject", bigIntegerObject, typeof bigIntegerObject);
---
+++ js/data types/task 2/index.js
@@ -0,0 +1,12 @@
+let name = "Marina";
+let age = 32;
+let femaleStatus = true;
+
+name = 42;
+alert(name);
+
+age = "Some text";
+alert(age);
+
+femaleStatus = null;
+alert(femaleStatus);
